2d7059df78716f568c2b6706c359506c6206ad8d
5solbin <dhrhfqls@naver.com>
2025-07-07 17:48:58 +0900

    회원가입 컨트롤러

---
+++ src/main/java/solbin/project/salary/controller/UserController.java
@@ -0,0 +1,42 @@
+package solbin.project.salary.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import solbin.project.salary.dto.ResponseDto;
+import solbin.project.salary.dto.join.JoinReqDto;
+import solbin.project.salary.dto.join.JoinResDto;
+import solbin.project.salary.service.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody JoinReqDto joinReqDto, BindingResult result) {
+
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+
+            for (FieldError error : result.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(new ResponseDto<>(-1, "유효성 검사 실패", errors), HttpStatus.BAD_REQUEST);
+        }
+
+        JoinResDto joinResDto = userService.join(joinReqDto);
+        return new ResponseEntity<>(new ResponseDto<>(1, "회원가입 성공", joinResDto), HttpStatus.OK);
+    }
+}
---
+++ src/test/java/solbin/project/salary/controller/UserControllerTest.java
@@ -0,0 +1,81 @@
+package solbin.project.salary.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import solbin.project.salary.dto.join.JoinReqDto;
+import solbin.project.salary.dummy.DummyObject;
+import solbin.project.salary.repository.UserRepository;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class UserControllerTest extends DummyObject {
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private EntityManager em;
+
+    @BeforeEach
+    public void setup() {
+        userRepository.save(newUser("bean@naver.com", "빈"));
+        em.clear();
+    }
+
+    @Test
+    public void 회원가입_성공() throws Exception{
+        //given
+        JoinReqDto dto = new JoinReqDto();
+        dto.setEmail("sol@naver.com"); // 추후 validataion 을 위해서 이메일 형식
+        dto.setPassword("password");
+        dto.setName("솔");
+
+        String requestBody = om.writeValueAsString(dto);
+
+        //when
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.post("/api/login")
+                        .content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        //then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    public void 회원가입_실패() throws Exception{
+        //given
+        JoinReqDto dto = new JoinReqDto();
+        dto.setEmail("bean@naver.com"); // 추후 validataion 을 위해서 이메일 형식
+        dto.setPassword("password");
+        dto.setName("솔");
+
+        String requestBody = om.writeValueAsString(dto);
+        //when
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.post("/api/login")
+                        .content(requestBody).contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+
+
+}
