13cf3951bba311ba5f9c406e9fb058f5161c3682
5solbin <dhrhfqls@naver.com>
2025-07-15 13:02:52 +0900

    swagger 설정

---
+++ src/main/java/solbin/project/salary/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package solbin.project.salary.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt,new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    public Info apiInfo() {
+        return new Info()
+                .title("API Test")
+                .description("회원가입/로그인, 근무 일지 작성, 편집 등의 REST API")
+                .version("1.0");
+    }
+}
--- src/main/java/solbin/project/salary/dto/worklog/monthly/GetMonthlyResDto.java
+++ src/main/java/solbin/project/salary/dto/worklog/monthly/GetMonthlyResDto.java
@@ -10,10 +10,12 @@ public class GetMonthlyResDto {
     private String date;
     private Map<String, DailyInfo> days;
     private Long totalTime;
+    private Long salary;
 
     public GetMonthlyResDto(String date, Map<String, DailyInfo> days, Long totalTime) {
         this.totalTime = totalTime;
         this.date = date;
         this.days = days;
+        this.salary = totalTime * 10000; // 시급은 고정 만원이라고 가정
     }
 }
